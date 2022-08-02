cb208fbacde70d4f301594665fa830dbeeaf7bd9
kfigaj <kfigaj87@gmail.com>
2022-08-02 22:55:59 +0200

    32 33

---
+++ 33-js-wyswietlanie-opisu/js/main.js
@@ -0,0 +1,14 @@
+let nowafunkcja = document.getElementById("opis-wyswietlenie");
+
+let object = document.querySelector("button");
+
+let myHeading = document.querySelector("button");
+
+myHeading.onclick = function () {
+  alert("it is alive!!!!!");
+};
+console.log(nowafunkcja);
+console.log(myHeading);
+console.log(object);
+
+//
