aaeec1ba9cd15c4d26ce48ba40a945162f3c7a01
kfigaj <kfigaj87@gmail.com>
2022-08-08 19:36:42 +0200

    modyfikacja zadanie 36

--- 36-js-sprawdzanie-liczby2/js/main.js
+++ 36-js-sprawdzanie-liczby2/js/main.js
@@ -2,7 +2,7 @@ let x = 5;
 let y = 15;
 
 function compare1(num1, num2) {
-  if ((x == 20, y == 20 || x + y <= 20)) {
+  if (x == 20 || y == 20 || x + y <= 20) {
     return true;
   } else {
     return false;
---
+++ 38-js-dzialania/js/main.js
@@ -0,0 +1,28 @@
+// Funkcja ma dodawać, odejmować i mnożyć liczby przekazane do parametrów i wyświetlać w konsoli następujące stringi:
+
+// 1. "Wynik dodawania wynosi x"
+// 2. "Wynik odejmowania wynosi x"
+// 3. "Wynik mnożenia wynosi x"
+
+// Gdzie x jest wynikiem danego działania.
+
+// Natomiast jeżeli wynikiem jest liczba ujemna, w konsoli powinien pojawić się string:
+// "Wynik jest nieprawidłowy"
+
+
+let element1 = 4
+let element2 = 12
+
+function action(num1, num2) {
+    let actions1 = (element1 + element2);
+    let actions2 = (element1 - element2);
+    let actions3 = (element1 * element2); {
+        if 
+    }
+
+
+}
+
+console.log(multiply(element1, elemant2))
+
+
